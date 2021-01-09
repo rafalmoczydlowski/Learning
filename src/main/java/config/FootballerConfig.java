b2ee16baa7469132c20b8e29f569c51b780b2cd7
rafalmoczydlowski <moczydlowski7@wp.pl>
2021-01-09 13:09:58 +0100

    Learning to write configuration and unit tests in Spring.

--- src/main/java/config/FootballerConfig.java
+++ src/main/java/config/FootballerConfig.java
@@ -1,9 +1,30 @@
 package config;
 
+/*/
+import com.example.demo.DefenderFootballer;
+import com.example.demo.DefenderFootballerStats;
+import com.example.demo.Footballer;
+import org.springframework.context.annotation.Bean;
+/*/
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 @ComponentScan("com.example.demo")
 public class FootballerConfig {
+
+    /*/ to byłoby potrzebne gdybym nie korzystał z adnotacji ComponentScan
+    @Bean
+    public Footballer defenderFootballer() {
+        return new DefenderFootballer();
+    }
+
+    @Bean
+    //wstrzykiwanie przez settera
+    public DefenderFootballerStats defenderFootballerStats(Footballer footballer) {
+        DefenderFootballerStats defenderFootballerStats = new DefenderFootballerStats(footballer);
+        defenderFootballerStats.setFootballer(footballer);
+        return defenderFootballerStats;
+    }
+    /*/
 }
