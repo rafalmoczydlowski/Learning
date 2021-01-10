bc281095ba35353bfae4b8767d6090c463e71387
rafalmoczydlowski <moczydlowski7@wp.pl>
2021-01-10 12:56:18 +0100

    Learning to write configuration and unit tests in Spring.

--- src/main/java/config/ExpressiveConfig.java
+++ src/main/java/config/ExpressiveConfig.java
@@ -1,12 +1,8 @@
 package config;
 
-import com.example.demo.DefenderFootballer;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.PropertySource;
-import org.springframework.core.env.Environment;
 
 @Configuration
 @PropertySource("classpath:/defender.properties")
